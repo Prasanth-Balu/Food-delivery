8cc2e5da853ab39fb6a3f912c4819e1fb858a09e
Prasanth-Balu <prasanthbm22@gmail.com>
2026-07-01 14:08:53 +0530

    Add files via upload

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,99 @@
+import basket_icon from './basket_icon.png'
+import logo from './weblogo.jpg'
+import search_icon from './search_icon.png'
+
+import menu1 from './menu1.png'
+import menu2 from './menu2.png'
+import menu3 from './menu3.png'
+import menu4 from './menu4.jpg'
+import menu5 from './menu5.png'
+import menu6 from './menu6.jpg'
+import menu7 from './menu7.jpg'
+import menu8 from './menu8.png'
+
+
+
+import add_icon_white from './add_icon_white.png'
+import add_icon_green from './add_icon_green.png'
+import remove_icon_red from './remove_icon_red.png'
+import app_store from './app_store.png'
+import play_store from './play_store.png'
+import linkedin_icon from './linkedin_icon.png'
+import facebook_icon from './facebook_icon.png'
+import twitter_icon from './twitter_icon.png'
+import cross_icon from './cross_icon.png'
+import selector_icon from './selector_icon.png'
+import rating_starts from './rating.png'
+import profile_icon from './profile_icon.png'
+import bag_icon from './bag_icon.png'
+import logout_icon from './logout_icon.png'
+import parcel_icon from './parcel_icon.png'
+
+export const assets = {
+    logo,
+    basket_icon,
+    search_icon,
+    rating_starts,
+    add_icon_green,
+    add_icon_white,
+    remove_icon_red,
+    app_store,
+    play_store,
+    linkedin_icon,
+    facebook_icon,
+    twitter_icon,
+    cross_icon,
+    selector_icon,
+    profile_icon,
+    logout_icon,
+    bag_icon,
+    parcel_icon
+}
+
+export const menu_list = [
+    {
+        menu_name: "Biriyani",
+        menu_image: menu1
+    },
+    {
+        menu_name: "Dosa",
+        menu_image: menu2
+    },
+    {
+        menu_name: "Fruit Juices",
+        menu_image: menu3
+    },
+    {
+        menu_name: "Veg Meals",
+        menu_image: menu4
+    },
+    {
+        menu_name: "Gravy",
+        menu_image: menu5
+    },
+    {
+        menu_name: "Shake",
+        menu_image: menu6
+    },
+    {
+        menu_name: "Cake",
+        menu_image: menu7
+    },
+    {
+        menu_name: "Pizza",
+        menu_image: menu8
+    }]
+
+
+
+
+
+
+export const food_list = [
+
+    {
+       
+    }
+
+ 
+]
